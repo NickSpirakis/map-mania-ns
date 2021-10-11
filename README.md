00eeb67e2b255c30c15979fb4da14da4b10fae0b
NickSpirakis <nicholasespirakis@lewisu.edu>
2021-10-10 23:59:12 -0500

    updated index

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # map-mania-ns
+# for adding 1 additional feature I found
+# this which is the syntax for adding listeners notifications to the screen: 
+# https://www.techstrikers.com/GoogleMap/Code/how-to-add-bounds-changed-event-listener-to-google-map.php
+# i thought it would be cool to see, and i was suprised how similar it was to just adding it to console, which I also have included for requirement 2
+# https://www.e-education.psu.edu/geog863_gmaps/l3_p7.html
+# this link showed me how to add a pin for my location
+# Nick Spirakis
+# Map Mania Version 1
+# nicholasespirakis@lewisu.edu
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var gMap;
 
 function initMap() {
+
   gMap = new google.maps.Map(document.getElementById("myMapID"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+
+  //requirement 2.5 interesting feature
 //the part that uses the get element by id are based off of the tutorial mentioned in readme
   gMap.addListener('bounds_changed', () => {
     document.getElementById('panel').innerHTML = 'Bounds Changed';
@@ -12,7 +15,32 @@ function initMap() {
       document.getElementById('panel').innerHTML = ' ';
     }, 2000);
     console.log('bounds changed');
+    console.log(gMap.getBounds());
   });
+
+//requirement 2.1
+  var loc = new google.maps.LatLng(41.6050, -88.0806);
+
+  //requirement 2.2, 2.3
+  gMap.addListener('in_bounds', () => {
+    if (gMap.getBounds().contains(loc)){
+      console.log('in bounds');
+    }
+    //console.log('in_bounds');
+    //console.log(gMap.getBounds().contains(loc));
+  });
+//requirement 2.4
+  gMap.addListener('zoom_changed', () => {
+    console.log('zoom changed');
+    console.log(gMap.getZoom());
+  });
+
+  var myMarkerOptions = {
+    position: loc,
+    map: gMap
+  };
+
+  var myMarker = new google.maps.Marker(myMarkerOptions);
 }
 
 function initApplication(){
