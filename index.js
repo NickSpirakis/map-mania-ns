9a0bc4ef82c38420c79b3fbc4ed7794d0bfd837a
NickSpirakis <nicholasespirakis@lewisu.edu>
2021-10-23 20:56:50 -0500

    added places and code for markers

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 var gMap;
 
+var favoritePlaces = 
+[
+{"content":"Canoe Bay, WI", "coordinates":{"lat":45.3306,"lng":-91.4918}, 
+        "iconImagePath":"https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png"},
+
+{"content":"Myrtle Beach, SC", "coordinates":{"lat":33.6891,"lng":-78.8867}, "iconImagePath":"flag.png"},
+
+{"content":"Prague, Czechia", "coordinates":{"lat":50.0755,"lng":14.4378}, "iconImagePath":"flag.png"},
+
+{"content":"Nelson, New Zealand", "coordinates":{"lat":-41.2706,"lng":173.2840}, "iconImagePath":"flag.png"},
+
+{"content":"Krakow, Poland", "coordinates":{"lat":50.0647,"lng":19.9450}, "iconImagePath":"flag.png"},
+
+{"content":"Zakopane, Poland", "coordinates":{"lat":49.2992,"lng":19.9496}, "iconImagePath":"flag.png"},
+
+{"content":"Florence, Italy", "coordinates":{"lat":43.7696,"lng":11.2558}, "iconImagePath":"four.png"},
+
+{"content":"King's Island, Ohio", "coordinates":{"lat":39.3837224936383, "lng":-84.26881692571592}, "iconImagePath":"three.png"},
+{"content":"<strong>#2: Half Price Books !<strong>", "coordinates":{"lat":41.618528279518635, "lng":-87.85250890675368}, "iconImagePath":"two.png"},
+{"content":"<strong>#1: Orland Park, IL !<strong>", "coordinates":{"lat":41.590808441443656, "lng":-87.88732785982376}, "iconImagePath":"one.png"}
+
+];
+
+var currentPlaceIndex = favoritePlaces.length-1;
+var currentPlace = favoritePlaces[currentPlaceIndex];
+
+
 function initMap() {
 
   gMap = new google.maps.Map(document.getElementById("myMapID"), {
@@ -21,30 +48,51 @@ function initMap() {
 //requirement 2.1
   var loc = new google.maps.LatLng(41.6050, -88.0806);
 
-  //requirement 2.2, 2.3
-  //gMap.addListener('in_bounds', () => {
-    //if (gMap.getBounds().contains(loc)){
-      //console.log('in bounds');
-    //}
-    //console.log('in_bounds');
-    //console.log(gMap.getBounds().contains(loc));
-    
-  //});
-
   google.maps.event.addListener(gMap, 'idle', function() {
       updateGame()
   });
 
   function updateGame(){
-    console.log('in bounds');
+    console.log('updateGame()');
+    var zoomLevel = gMap.getZoom();
+    console.log("Zoom Level: " + zoomLevel);
+
     var inBounds = false;
-    if (gMap.getBounds().contains({lat:41.6050,lng:-88.0806})){
+    console.log("coords:" + JSON.stringify(currentPlace.coordinates));
+
+    if (gMap.getBounds().contains(currentPlace.coordinates))
+    {
       inBounds = true;
+      console.log("inbounds");
     }
-    console.log("inbounds:"+inBounds);
+    
+    if ((zoomLevel > 7)&&(inBounds))
+    {
+      console.log("Location Found!");
+      addMarker(currentPlace);
+      nextPlace();
+    }
+
   };
 
+  function nextPlace() {
+    currentPlaceIndex--;
+    currentPlace = favoritePlaces[currentPlaceIndex];
+  }
+
+  function addMarker(markerContent) {
+    var marker = new google.maps.Marker({position:markerContent.coordinates, map:gMap});
+    if (markerContent.iconImagePath) {
+        marker.setIcon(markerContent.iconImagePath);
+    }
+
+    if (markerContent.content) {
+        var infoWindow = new google.maps.InfoWindow({"content":markerContent.content});
+        marker.addListener("click", function() { infoWindow.open(gMap, marker) });
+    }
+}
 //requirement 2.4
+/*
   gMap.addListener('zoom_changed', () => {
     console.log('zoom changed');
     console.log(gMap.getZoom());
@@ -56,28 +104,11 @@ function initMap() {
   };
 
   var myMarker = new google.maps.Marker(myMarkerOptions);
-
-  /* modal learned from w3schools tutorial*/
-/*
-  var modal = document.getElementById("myModal");
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-  btn.onclick = function(){
-    modal.style.display = "block";
-  }
-
-  span.onclick = function(){
-    modal.style.display = "none";
-  }
-
-  window.onclick = function(event) {
-    if (event.target == modal){
-      modal.style.display = "none";
-    }
-  }
 */
 
 }
+
+
 //initApp
 function initApplication(){
   console.log('Map Mania Lite - Starting!');
